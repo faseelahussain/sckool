7f68fef0548f15f749fca0a6c7a1c35418fd7357
faseelahussain <faseelahussain979@gmail.com>
2024-08-05 16:32:23 +0530

    new

---
+++ .next/static/chunks/pages/admin-47c0730d3fad9886.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[964],{8195:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/admin",function(){return u(870)}])}},function(n){n.O(0,[233,85,888,774,179],function(){return n(n.s=8195)}),_N_E=n.O()}]);
---
+++ .next/static/chunks/pages/admin/adminDashboard-3bd96a7c5118df44.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[983],{2009:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/admin/adminDashboard",function(){return u(1536)}])}},function(n){n.O(0,[233,85,888,774,179],function(){return n(n.s=2009)}),_N_E=n.O()}]);
